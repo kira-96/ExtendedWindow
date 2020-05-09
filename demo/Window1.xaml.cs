99b2a21070bc4f853f39ce9c0cfc15c13743b6b7
yzh <yangzhenghan@lonwin.org>
2020-05-09 13:50:38 +0800

    添加新的示例

---
+++ demo/Window1.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace demo
+{
+    /// <summary>
+    /// Window1.xaml 的交互逻辑
+    /// </summary>
+    public partial class Window1
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+    }
+}
